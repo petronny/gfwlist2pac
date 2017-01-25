f6079d624259779435d317ae9f6ddec357785aeb
Jingbei Li <i@jingbei.li>
2017-01-25 23:10:40 +0800

    fix calil.sh

--- calil.sh
+++ calil.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+cd `dirname $0`
+git reset --hard
+git pull
+
 git submodule update --init
 cd gfwlist
 git pull origin master
 cd ..
+
 git add .
 git commit -m "[$(LANG=C date)]auto update"
 git push origin master
