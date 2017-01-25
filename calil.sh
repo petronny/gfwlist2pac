0409ffcaa36917d64fb68c9767a9871474a5be26
Jingbei Li <i@jingbei.li>
2017-01-25 23:17:27 +0800

    use genpac to gen gfwlist.pac

--- calil.sh
+++ calil.sh
@@ -6,6 +6,7 @@ git pull
 git submodule update --init
 cd gfwlist
 git pull origin master
+genpac --proxy="SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" -o ../gfwlist.pac --gfwlist-local=gfwlist.txt
 cd ..
 
 git add .
