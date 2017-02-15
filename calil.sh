863dc7f0777beda861c795b6a50e638fb9665971
Jingbei Li <i@jingbei.li>
2017-02-15 14:49:17 +0800

    delete generated date

--- calil.sh
+++ calil.sh
@@ -8,6 +8,7 @@ cd gfwlist
 git pull origin master
 ~/.local/bin/genpac --proxy="SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" -o ../gfwlist.pac --gfwlist-local=gfwlist.txt
 cd ..
+sed '3d' -i gfwlist.pac
 
 git add .
 git commit -m "[$(LANG=C date)]auto update"
