19548e43d8c35fcad9bda6ba55534e2e1497cbc0
Jingbei Li <i@jingbei.li>
2017-10-15 14:36:27 +0800

    use virtual python env now

--- calil.sh
+++ calil.sh
@@ -4,11 +4,18 @@ git reset --hard
 git pull
 
 git submodule update --init
-cd gfwlist
-git pull origin master
-~/.local/bin/genpac --proxy="SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" -o ../gfwlist.pac --gfwlist-local=gfwlist.txt
-cd ..
+for i in gfwlist genpac
+do
+	(cd $i;git pull origin master)
+done
+
+virtualenv2 env
+source env/bin/activate
+(cd genpac;python2 setup.py install)
+
+env/bin/genpac --proxy="SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" -o gfwlist.pac --gfwlist-local=gfwlist/gfwlist.txt
 sed '3d' -i gfwlist.pac
+deactivate
 
 git add .
 git commit -m "[$(LANG=C date)]auto update"
